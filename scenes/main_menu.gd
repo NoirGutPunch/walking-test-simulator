ca10b4ba3d711291c837f8ebc5c7d931831a0e84
MatchaMingle <noirgutpunch@gmail.com>
2025-12-26 14:39:44 -0500

    Main menu controls

---
+++ scenes/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
+
+func _ready() -> void:
+	main_buttons.visible = true
+	options.visible = false
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+
+func _on_option_button_pressed() -> void:
+	main_buttons.visible = false
+	options.visible = true
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_button_pressed() -> void:
+	_ready()
